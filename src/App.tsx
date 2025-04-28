16b95d251ef2b830cb95359755984ec88e35f8b0
Cody Lewis <14844346+codylewis@users.noreply.github.com>
2025-04-28 11:39:37 -0500

    Update layout + add attribution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import "./App.css";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import { Paper, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Link,
+  Paper,
+  Typography,
+} from "@mui/material";
 import { IRISH_WORD_FREQUENCY } from "./IRISH_WORD_FREQUENCY";
 
 const columns: GridColDef[] = [
   { field: "rank", headerName: "Rank", type: "number" },
-  { field: "lemma", headerName: "Word", type: "string" },
-  { field: "frequency", headerName: "Frequency", type: "number" },
+  { field: "lemma", headerName: "Word", type: "string", flex: 1 },
+  { field: "frequency", headerName: "Frequency", type: "number", width: 125 },
 ];
 
 function App() {
@@ -14,10 +22,41 @@ function App() {
 
   return (
     <>
-      <Typography variant="body1" gutterBottom>
-        Irish Word Frequency
-      </Typography>
-      <Paper sx={{ height: 400, width: "100%" }}>
+      <Card variant="outlined">
+        <CardContent>
+          <Typography variant="body1" gutterBottom>
+            Irish Word Frequency
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            This is a list of approximately 6,500 Irish lemmas (words) ordered
+            by frequency of use.{" "}
+            <Link href="https://cadhan.com/lsg/">Michal Měchura</Link> extracted
+            this dataset from the Irish half of the{" "}
+            <Link href="https://corpas.focloir.ie/">
+              New Corpus for Ireland
+            </Link>{" "}
+            up and then cleaned up by cross-checking against a large-coverage
+            lexicon (Kevin Scannell's{" "}
+            <Link href="https://cadhan.com/lsg/">
+              Líonra Séimeantach na Gaeilge
+            </Link>
+            ). It was been made available for use under the{" "}
+            <Link href="https://opendatacommons.org/licenses/odbl/summary/">
+              Open Database License
+            </Link>
+            .
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            size="small"
+            href="https://github.com/michmech/irish-word-frequency"
+          >
+            Source for dataset
+          </Button>
+        </CardActions>
+      </Card>
+      <Paper>
         <DataGrid
           rows={IRISH_WORD_FREQUENCY}
           columns={columns}
@@ -26,6 +65,12 @@ function App() {
           showToolbar
           sx={{ border: 0 }}
           getRowId={({ rank }) => rank}
+          sortModel={[
+            {
+              field: "frequency",
+              sort: "desc",
+            },
+          ]}
         />
       </Paper>
     </>
