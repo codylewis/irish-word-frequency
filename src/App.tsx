17d596d92043c4d645cb11c905c5f8d46e3b2813
Cody Lewis <14844346+codylewis@users.noreply.github.com>
2025-04-28 11:18:06 -0500

    Add data

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
 import "./App.css";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import { Paper } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
+import { IRISH_WORD_FREQUENCY } from "./IRISH_WORD_FREQUENCY";
 
 const columns: GridColDef[] = [
   { field: "rank", headerName: "Rank", type: "number" },
-  { field: "word", headerName: "Word", type: "string" },
+  { field: "lemma", headerName: "Word", type: "string" },
   { field: "frequency", headerName: "Frequency", type: "number" },
 ];
-
-const rows: string[] = [];
 
 function App() {
   const paginationModel = { page: 0, pageSize: 5 };
 
   return (
     <>
+      <Typography variant="body1" gutterBottom>
+        Irish Word Frequency
+      </Typography>
       <Paper sx={{ height: 400, width: "100%" }}>
         <DataGrid
-          rows={rows}
+          rows={IRISH_WORD_FREQUENCY}
           columns={columns}
           initialState={{ pagination: { paginationModel } }}
           pageSizeOptions={[5, 10]}
-          checkboxSelection
+          showToolbar
           sx={{ border: 0 }}
+          getRowId={({ rank }) => rank}
         />
       </Paper>
     </>
